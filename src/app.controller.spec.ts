406adf3fae0ab23d884f8493673243d32cd5d48d
Xiaoai Zhu <xzhu411@gatech.edu>
2026-04-03 05:11:09 -0400

    updated mesasges

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -13,9 +13,9 @@ describe('AppController', () => {
   });
 
   describe('getHello', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "check out localhost:3000/api#/ to see the swagger UI"', () => {
       const appController = app.get(AppController);
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('check out localhost:3000/api#/ to see the swagger UI');
     });
   });
 });
